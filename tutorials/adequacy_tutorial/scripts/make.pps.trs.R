a5de3ac3be26c17848d5eecab10d0dbaa911f14a
Louis du Plessis <Laduplessis@gmail.com>
2016-10-03 18:25:15 +0100

    New Site Build

---
+++ tutorials/adequacy_tutorial/scripts/make.pps.trs.R
@@ -0,0 +1,27 @@
+# This function processes simulated datasets created with the function make.pps.als. It takes the posterior phylograms and simulated alignments. The function creates the posterior predictive simulated distribution of phylograms. this function also requires the empirical dataset and the assumed topology.
+
+make.pps.tr <- function(sims, empdat, topo, model = "JC"){
+	 topo$edge.length <- rep(1, length(topo$edge.length))
+	 if(model == "JC"){
+	 	 emphy <- optim.pml(pml(topo, empdat))
+	 } else if(model == "GTR+G"){
+	   	 emphy <- optim.pml(pml(topo, empdat), optBf=FALSE, optQ=FALSE, optGamma = T)
+	 }
+	 empmlik <- multlik(empdat)
+	 empbl <- emphy$tree$edge.length
+	 simbl <- emphy$tree$edge.length
+	 simultlik <- vector()
+	 for(i in 1:length(sims)){
+	       if(model == "JC"){
+	       		ppsphy <- optim.pml(pml(topo, sims[[i]][[3]]))
+	       } else if(model == "GTR+G"){
+	       	        ppsphy <- optim.pml(pml(topo, sims[[i]][[3]]), optBf=FALSE, optQ=FALSE, optGamma = T)
+	       }
+	       simbl <- rbind(simbl, ppsphy$tree$edge.length)
+	       simultlik[i] <- multlik(sims[[i]][[3]])
+	       print(paste("Simulation", i, "processed"))
+	 }
+	 simbl <- simbl[2:nrow(simbl),]
+	 return(list(empbl, simbl, empmlik, simultlik))
+
+}
